3a9c6751c76e2422288a0eb84b74994a9c54fee3
Thea Calaquian <tc.11927@Tristans-MacBook-Pro.local>
2024-09-09 10:57:07 -0700

    more changes

--- test.js
+++ test.js
@@ -4,13 +4,12 @@ console.log("weeeeeeeeeee what a good story")
 
 console.log("another change to the code wowowoowowow")
 
+
 console.log("\x1b[32m", "Hi")
-
 console.log("\x1b[0m")
-
 console.log("\x1b[35m", "cool words")
-
 console.log("\x1b[0m")
-
 console.log("\x1b[44m", "Hello")
+console.log("\x1b[0m")
+console.log("\x1b[40m", "woahh spooky")
 
