0961b429b771e7a98a19e57b9034867b2281c126
Thea Calaquian <tc.11927@Tristans-MacBook-Pro.local>
2024-09-09 10:47:55 -0700

    added green text

--- test.js
+++ test.js
@@ -3,3 +3,5 @@ console.log("Welcome to the amazing Metal Person");
 console.log("weeeeeeeeeee what a good story")
 
 console.log("another change to the code wowowoowowow")
+
+console.log("\x1b[32m", "Hi")
