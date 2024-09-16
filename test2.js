8547965fc2ab5b101b1e5e91278706ed5da706d6
Thea Calaquian <tc.11927@Tristans-MacBook-Pro.local>
2024-09-16 09:13:46 -0700

    anotherrrr

---
+++ test2.js
@@ -0,0 +1,11 @@
+const animal = "cats";
+
+let action = "playful";
+
+console.log(" "+ animal +"  are captivating creatures, with their graceful movements and curious behaviors. Each "+ animal +" has its own unique charm, but all "+ animal +" share a common bond: their " +action+ " nature. " +action+ " "+ animal +" delight in pouncing and chasing, turning any ordinary moment into an adventure. The " +action+ " antics of "+ animal +" can bring joy and laughter, reminding us of the simple pleasures in life. Whether lounging in the sun or exploring new corners of the house, "+ animal +" are endlessly entertaining. Their " +action+ " spirit never seems to wane, making every day with a "+ animal +" a little more magical.");
+
+console.log("-----------------second story----------------------")
+
+action = "lazy";
+
+console.log(" "+ animal +"  are captivating creatures, with their graceful movements and curious behaviors. Each "+ animal +" has its own unique charm, but all "+ animal +" share a common bond: their " +action+ " nature. " +action+ " "+ animal +" delight in pouncing and chasing, turning any ordinary moment into an adventure. The " +action+ " antics of "+ animal +" can bring joy and laughter, reminding us of the simple pleasures in life. Whether lounging in the sun or exploring new corners of the house, "+ animal +" are endlessly entertaining. Their " +action+ " spirit never seems to wane, making every day with a "+ animal +" a little more magical.");
